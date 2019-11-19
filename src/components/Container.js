dc6e2dbe5888dd673e2d76f06873c07004171c52
tomekgora <tomekgora@gmail.com>
2019-11-19 17:51:02 +0100

    started writing fetch request

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,6 +3,19 @@ import CrimeRecord from "./CrimeRecord.js"
 import LikeCounter from "./LikeCounter.js"
 
 export default class Container extends Component {
+   
+    state = {
+        loading: true
+    }
+
+    componentDidMount() {
+        return fetch("https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2017-01")
+        .then(res => res.json())
+        .then(data => {
+            return <div>(Render stuff inside here)</div>;
+        })
+    }
+
     render () {
         return <div className="container">
             <h1>SHOULD YOU BE AFRAID?</h1>
--- src/components/CrimeRecord.js
+++ src/components/CrimeRecord.js
@@ -2,8 +2,23 @@ import React, { Component } from "react";
 import LikeCounter from "./LikeCounter.js"
 
 export default class CrimeRecord extends Component {
+
+    // state =  {
+    //     loading: true
+    //     articles:
+    //     error: 
+    // }
+
+    // https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2017-01
+
     render () {
         return <div className ="crime-record">
+            <p className="heading">Crime Record</p>
+            <p>Category:</p>
+            <p>Type:</p>
+            <p></p>
+            <p></p>
+
             <LikeCounter/>
 
 {/* Category:, location-type: category-image:
