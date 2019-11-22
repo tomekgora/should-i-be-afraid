385ab5670800419b969feeed3907bad1d9fc17dc
tomekgora <tomekgora@gmail.com>
2019-11-22 09:32:12 +0100

    going to branch

--- src/components/Container.js
+++ src/components/Container.js
@@ -12,11 +12,15 @@ export default class Container extends Component {
         return fetch("https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2017-01")
         .then(res => res.json())
         .then(data => {
-            return <div>(Render stuff inside here)</div>;
+            // return <div>(Render stuff inside here)</div>;
+            // setState()
         })
     }
 
     render () {
+        // if we dont have data display loading
+
+        // if we have data display the data (this.state.records.map)
         return <div className="container">
             <h1>SHOULD YOU BE AFRAID?</h1>
             <button>Click to find out</button>
